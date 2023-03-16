5e2f2d314f1140baa5a0f4f0fcf4507b3303ef1a
AirineiAndrei <acairinei17@gmail.com>
2023-03-16 13:35:04 +0200

    Printing

--- src/Printing.hs
+++ src/Printing.hs
@@ -5,8 +5,15 @@ import Exp
 import Data.List (intercalate)
 
 showVar :: Var -> String
-showVar = undefined
+showVar var = getVar var
 
 showExp :: ComplexExp -> String
-showExp = undefined
+showExp (CX var) = showVar var
+showExp (Nat nat) = show nat
+showExp (CLam inLmabda outLambda) = "\\" ++ (showVar inLmabda) ++ " -> " ++ (showExp outLambda)
+showExp (CApp exp1 exp2) = (showExp exp1) ++ " " ++ (showExp exp2)
+showExp (Let x val exp) = "let " ++ (showVar x) ++ " = " ++  (showExp val) ++ " in " ++ (showExp exp)
+showExp (LetRec x val exp) = "letrec " ++ (showVar x) ++ " = " ++  (showExp val) ++ " in " ++ (showExp exp)
+showExp (List vals) = (show (map showExp vals))
+showExp _ = "undefined"
 

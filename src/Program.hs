b306eabf6eea4cda0b94539e8231a170cf19b0ed
AirineiAndrei <acairinei17@gmail.com>
2023-04-13 21:19:10 +0300

    lab6

--- src/Program.hs
+++ src/Program.hs
@@ -1,7 +1,7 @@
 
 module Program where
 import Exp
-import Lab2 ( Parser, endOfInput, whiteSpace, reserved, semiSep1 )
+import Lab2 ( Parser, endOfInput, whiteSpace, reserved, semiSep1,semi )
 import Parsing ( expr, var, parseFirst )
 import Sugar ( desugarExp, desugarVar )
 import Eval ( substitute )
@@ -18,7 +18,11 @@ data Definition = Definition
   deriving (Show)
 
 definition :: Parser Definition
-definition = undefined
+definition = do
+  headVar <- var
+  args <- many var
+  body <- reserved ":=" >> expr
+  return $ Definition headVar args body
 
 -- >>> parseFirst definition "id := \\x -> x"
 -- Just (Definition {defHead = Var {getVar = "id"}, defArgs = [], defBody = CLam (Var {getVar = "x"}) (CX (Var {getVar = "x"}))})
@@ -30,7 +34,11 @@ definition = undefined
 -- Just (Definition {defHead = Var {getVar = "const"}, defArgs = [Var {getVar = "x"},Var {getVar = "y"}], defBody = CX (Var {getVar = "x"})})
 
 program :: Parser [Definition]
-program = undefined
+program = do
+  whiteSpace
+  lines <- many (definition <* semi)
+  endOfInput
+  return lines
 
 -- >>> parseFirst program "    id x := x ; const x y := x"
 -- Nothing
@@ -39,7 +47,8 @@ program = undefined
 -- Just [Definition {defHead = Var {getVar = "id"}, defArgs = [Var {getVar = "x"}], defBody = CX (Var {getVar = "x"})},Definition {defHead = Var {getVar = "const"}, defArgs = [Var {getVar = "x"},Var {getVar = "y"}], defBody = CX (Var {getVar = "x"})}]
 
 definitionExp :: Definition -> ComplexExp
-definitionExp def = undefined
+definitionExp (Definition headVar [] bodyExp) = bodyExp
+definitionExp (Definition headVar (headArgs:leftArgs) bodyExp) = CLam headArgs (definitionExp (Definition headVar leftArgs bodyExp))
 
 -- >>> definitionExp (Definition {defHead = Var {getVar = "const"}, defArgs = [Var {getVar = "x"},Var {getVar = "y"}], defBody = CX (Var {getVar = "x"})})
 -- CLam (Var {getVar = "x"}) (CLam (Var {getVar = "y"}) (CX (Var {getVar = "x"})))
@@ -47,8 +56,8 @@ definitionExp def = undefined
 type Environment = Map.Map IndexedVar Exp
 
 programEnv :: [Definition] -> Environment
-programEnv pgm = undefined
+programEnv pgm = Map.fromList (zip (map desugarVar(map defHead pgm)) (map desugarExp (map definitionExp pgm)))
 
 normalizeEnv :: Environment -> Exp -> Exp
-normalizeEnv = undefined
+normalizeEnv env exp = exp 
 
